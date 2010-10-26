1a32e964eb2fc8f23f1fb1f2336c146abcccb266
David M. Syzdek <syzdek@bindlebinaries.com>
2010-10-26 15:46:14 -0800

    Slowly adding files to the openssl targets for building.

--- ports/security/openssl/build-aux/configure-wrapper.sh
+++ ports/security/openssl/build-aux/configure-wrapper.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
-PATH=/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin:${PATH}
-
-./configure --host=arm-apple-darwin10 \
-   CC=/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/arm-apple-darwin10-gcc-4.2.1 \
-   LD=/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/ld \
-   LIBTOOL=/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/libtool  \
-   LDFLAGS=-L/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS4.1.sdk/usr/lib \
-   CPPFLAGS=-I/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS4.1.sdk/usr/include \
-   --prefix=/tmp/iosports --enable-static --disable-shared \
+./config --openssldir=/tmp/iOSPorts/ \
+   no-threads no-shared no-asm no-zlib no-dso no-krb5  \
    $@ || exit $?
 
---
+++ ports/security/openssl/examples/other/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  test
+//
+//  Created by David Syzdek on 10/26/10.
+//  Copyright 2010 David M. Syzdek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
+
