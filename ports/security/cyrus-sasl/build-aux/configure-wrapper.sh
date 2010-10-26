ea75d8c416f23795709d675a76e9fea672f4f84f
David M. Syzdek <syzdek@bindlebinaries.com>
2010-10-26 11:36:25 -0800

    Adding initial Cyrus SASL files.

---
+++ ports/security/cyrus-sasl/build-aux/configure-wrapper.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+PATH=/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin:${PATH}
+
+./configure --host=arm-apple-darwin10 \
+   CC=/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/arm-apple-darwin10-gcc-4.2.1 \
+   LD=/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/ld \
+   LIBTOOL=/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/libtool  \
+   LDFLAGS=-L/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS4.1.sdk/usr/lib \
+   CPPFLAGS=-I/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS4.1.sdk/usr/include \
+   --prefix=/tmp/iosports --enable-static --disable-shared \
+   --without-openssl \
+   --disable-passdss --disable-ntlm --disable-otp --disable-srp --disable-java  \
+   --disable-digest $@ || exit $?
+
+gcc -o include/makemd5 include/makemd5.c || exit $?
