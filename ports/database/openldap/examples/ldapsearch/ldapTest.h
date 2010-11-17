5d067bddbc91c78c3356000023c6b4329ef6a1f9
David M. Syzdek <syzdek@bindlebinaries.com>
2010-11-17 14:07:54 -0900

    Adding initial attempt at LDAP SASL auth demo.

--- ports/database/openldap/examples/ldapsearch/ldapTest.h
+++ ports/database/openldap/examples/ldapsearch/ldapTest.h
@@ -8,7 +8,24 @@
  */
 #include <ldap.h>
 
+typedef struct my_ldap_auth MyLDAPAuth;
+struct my_ldap_auth
+{
+   char * mech;
+   char * authuser;
+   char * user;
+   char * realm;
+   char * passwd;
+};
+
 void test_all_ldap(void);
 
 void test_simple_ldap(int version, const char * ldapURI, const char * bindDN,
    const char * bindPW, const char * baseDN, const char * filter, int scope);
+
+void test_sasl_ldap(int version, const char * ldapURI, const char * user,
+   const char * realm, const char * pass, const char * mech,
+   const char * baseDN, const char * filter, int scope);
+
+int my_ldap_sasl_interact_proc(LDAP *ld, unsigned flags, void *defaults,
+   void *sasl_interact);
--- ports/database/openldap/examples/ldapsearch/ldapTest.m
+++ ports/database/openldap/examples/ldapsearch/ldapTest.m
@@ -10,6 +10,7 @@
 #include "ldapTest.h"
 #include <stdlib.h>
 #include <string.h>
+#include <sasl/sasl.h>
 
 void test_all_ldap()
 {
@@ -21,6 +22,18 @@ void test_all_ldap()
       "o=test",               // LDAP search base DN
       "(objectclass=*)",      // LDAP search filter
       LDAP_SCOPE_SUB          // LDAP search scope
+   );
+
+   test_sasl_ldap(
+      LDAP_VERSION3,          // LDAP Protocol Version
+      "ldap://10.0.1.3",      // LDAP URI
+      "syzdek",               // SASL User
+      NULL,                   // SASL Realm
+      "drowssap",             // SASL password
+      "DIGEST-MD5",           // SASL mechanism
+      "o=test",               // LDAP Search Base DN
+      "(objectclass=*)",      // LDAP Search Filter
+      LDAP_SCOPE_SUB          // LDAP Search Scope
    );
 
    return;
@@ -45,60 +58,61 @@ void test_simple_ldap(int version, const char * ldapURI, const char * bindDN,
    servercredp     = NULL;
    dn              = "cn=Directory Manager";
 
-   NSLog(@"initialzing LDAP...");
+   NSLog(@"attempting simple bind:");
+   NSLog(@"   initialzing LDAP...");
    ldapURI = ldapURI ? ldapURI : "ldap://127.0.0.1";
    err = ldap_initialize(&ld, ldapURI);
    if (err != LDAP_SUCCESS)
    {
-      printf("ldap_initialize(): %s\n", ldap_err2string(err));
+      NSLog(@"   ldap_initialize(): %s\n", ldap_err2string(err));
       return;
    };
 
    if (version)
    {
-      NSLog(@"setting protocol version %i...", version);
+      NSLog(@"   setting protocol version %i...", version);
       err = ldap_set_option(ld, LDAP_OPT_PROTOCOL_VERSION, &version);
       if (err != LDAP_SUCCESS)
       {
-         printf("ldap_set_option(): %s\n", ldap_err2string(err));
+         NSLog(@"   ldap_set_option(): %s\n", ldap_err2string(err));
          ldap_unbind_ext_s(ld, NULL, NULL);
          return;
       };
    };
 
-   NSLog(@"binding to LDAP server...");
-   cred.bv_val = bindPW ? bindPW : NULL;
+   NSLog(@"   binding to LDAP server...");
+   cred.bv_val = bindPW ? strdup(bindPW) : NULL;
    cred.bv_len = bindPW ? (size_t) strlen("drowssap") : 0;
    err = ldap_sasl_bind_s(ld, bindDN, LDAP_SASL_SIMPLE, &cred, NULL, NULL, &servercredp);
    if (err != LDAP_SUCCESS)
    {
-      NSLog(@"ldap_sasl_bind_s(): %s", ldap_err2string(err));
+      NSLog(@"   ldap_sasl_bind_s(): %s", ldap_err2string(err));
       ldap_unbind_ext_s(ld, NULL, NULL);
       return;
    };
 
-   NSLog(@"initiating lookup...");
+   NSLog(@"   initiating lookup...");
    if ((err = ldap_search_ext_s(ld, baseDN, scope, filter, NULL, 0, NULL, NULL, NULL, -1, &res)))
    {
-      NSLog(@"ldap_search_ext_s(): %s", ldap_err2string(err));
+      NSLog(@"   ldap_search_ext_s(): %s", ldap_err2string(err));
       ldap_unbind_ext_s(ld, NULL, NULL);
       return;
    };
 
-   NSLog(@"checking for results...");
+   NSLog(@"   checking for results...");
    if (!(ldap_count_entries(ld, res)))
    {
-      NSLog(@"no entries found.");
+      NSLog(@"   no entries found.");
       ldap_msgfree(res);
       ldap_unbind_ext_s(ld, NULL, NULL);
       return;
    };
-   NSLog(@"%i entries found.", ldap_count_entries(ld, res));
+   NSLog(@"   %i entries found.", ldap_count_entries(ld, res));
 
-   NSLog(@"retrieving results...");
+   NSLog(@"   retrieving results...");
    if (!(entry = ldap_first_entry(ld, res)))
    {
-      NSLog(@"ldap_first_entry(): %s", ldap_err2string(err));
+      NSLog(@"   ldap_first_entry(): %s", ldap_err2string(err));
       ldap_msgfree(res);
       ldap_unbind_ext_s(ld, NULL, NULL);
       return;
@@ -107,7 +121,7 @@ void test_simple_ldap(int version, const char * ldapURI, const char * bindDN,
    while(entry)
    {
       NSLog(@" ");
-      NSLog(@"   dn: %s", ldap_get_dn(ld, entry));
+      NSLog(@"      dn: %s", ldap_get_dn(ld, entry));
 
       attribute = ldap_first_attribute(ld, entry, &ber);
       while(attribute)
@@ -115,7 +129,7 @@ void test_simple_ldap(int version, const char * ldapURI, const char * bindDN,
          if ((vals = ldap_get_values_len(ld, entry, attribute)))
          {
             for(i = 0; vals[i]; i++)
-               NSLog(@"   %s: %s", attribute, vals[i]->bv_val);
+               NSLog(@"      %s: %s", attribute, vals[i]->bv_val);
             ldap_value_free_len(vals);
          };
          ldap_memfree(attribute);
@@ -127,11 +141,202 @@ void test_simple_ldap(int version, const char * ldapURI, const char * bindDN,
    };
    NSLog(@" ");
 
-   NSLog(@"unbinding from LDAP server...");
+   NSLog(@"   unbinding from LDAP server...");
    ldap_unbind_ext_s(ld, NULL, NULL);
 	
 	return;
-
-
 }
 
+
+void test_sasl_ldap(int version, const char * ldapURI, const char * user,
+   const char * realm, const char * pass, const char * mech,
+   const char * baseDN, const char * filter, int scope)
+{
+   int              i;
+   int              err;
+   char           * attribute;
+   LDAP           * ld;
+   BerValue       * servercredp;
+   BerElement     * ber;
+   const char     * dn;
+   LDAPMessage    * res;
+   LDAPMessage    * entry;
+   struct berval ** vals;
+   MyLDAPAuth       auth;
+
+   vals            = NULL;
+   servercredp     = NULL;
+   dn              = "cn=Directory Manager";
+
+   NSLog(@"attempting SASL bind:");
+   NSLog(@"   initialzing LDAP...");
+   ldapURI = ldapURI ? ldapURI : "ldap://127.0.0.1";
+   err = ldap_initialize(&ld, ldapURI);
+   if (err != LDAP_SUCCESS)
+   {
+      NSLog(@"   ldap_initialize(): %s\n", ldap_err2string(err));
+      return;
+   };
+
+   version = version ? version : LDAP_VERSION3;
+   NSLog(@"   setting protocol version %i...", version);
+   err = ldap_set_option(ld, LDAP_OPT_PROTOCOL_VERSION, &version);
+   if (err != LDAP_SUCCESS)
+   {
+      NSLog(@"   ldap_set_option(): %s\n", ldap_err2string(err));
+      ldap_unbind_ext_s(ld, NULL, NULL);
+      return;
+   };
+
+   memset(&auth, 0, sizeof(MyLDAPAuth));
+   auth.mech      = mech  ? strdup(mech)   : NULL;
+   auth.authuser  = user  ? strdup(user)   : NULL;
+   auth.realm     = realm ? strdup(realm)  : NULL;
+   auth.passwd    = pass  ? strdup(pass)   : NULL;
+
+   if (!(auth.mech))
+      ldap_get_option(ld, LDAP_OPT_X_SASL_MECH,  &auth.mech);
+   if (!(auth.authuser))
+      ldap_get_option(ld, LDAP_OPT_X_SASL_AUTHCID, &auth.authuser);
+   if (!(auth.realm))
+      ldap_get_option(ld, LDAP_OPT_X_SASL_REALM, &auth.realm);
+   ldap_get_option(ld, LDAP_OPT_X_SASL_AUTHZID, &auth.user);
+
+NSLog(@"   Bind Data:");
+NSLog(@"      Mech:      %s", auth.mech     ? auth.mech     : "(NULL)");
+NSLog(@"      User:      %s", auth.user     ? auth.user     : "(NULL)");
+NSLog(@"      Auth User: %s", auth.authuser ? auth.authuser : "(NULL)");
+NSLog(@"      Realm:     %s", auth.realm    ? auth.realm    : "(NULL)");
+NSLog(@"      Passwd:    %s", auth.passwd   ? auth.passwd   : "(NULL)");
+
+   NSLog(@"   binding to LDAP server...");
+   err = ldap_sasl_interactive_bind_s(ld, NULL, NULL,
+              NULL, NULL,
+              LDAP_SASL_QUIET, my_ldap_sasl_interact_proc,
+              &auth);
+
+   if (err != LDAP_SUCCESS)
+   {
+      NSLog(@"   ldap_sasl_bind_s(): %s", ldap_err2string(err));
+      ldap_unbind_ext_s(ld, NULL, NULL);
+      return;
+   };
+
+   NSLog(@"   initiating lookup...");
+   if ((err = ldap_search_ext_s(ld, baseDN, scope, filter, NULL, 0, NULL, NULL, NULL, -1, &res)))
+   {
+      NSLog(@"   ldap_search_ext_s(): %s", ldap_err2string(err));
+      ldap_unbind_ext_s(ld, NULL, NULL);
+      return;
+   };
+
+   NSLog(@"   checking for results...");
+   if (!(ldap_count_entries(ld, res)))
+   {
+      NSLog(@"   no entries found.");
+      ldap_msgfree(res);
+      ldap_unbind_ext_s(ld, NULL, NULL);
+      return;
+   };
+   NSLog(@"   %i entries found.", ldap_count_entries(ld, res));
+
+   NSLog(@"   retrieving results...");
+   if (!(entry = ldap_first_entry(ld, res)))
+   {
+      NSLog(@"   ldap_first_entry(): %s", ldap_err2string(err));
+      ldap_msgfree(res);
+      ldap_unbind_ext_s(ld, NULL, NULL);
+      return;
+   };
+
+   while(entry)
+   {
+      NSLog(@" ");
+      NSLog(@"      dn: %s", ldap_get_dn(ld, entry));
+
+      attribute = ldap_first_attribute(ld, entry, &ber);
+      while(attribute)
+      {
+         if ((vals = ldap_get_values_len(ld, entry, attribute)))
+         {
+            for(i = 0; vals[i]; i++)
+               NSLog(@"      %s: %s", attribute, vals[i]->bv_val);
+            ldap_value_free_len(vals);
+         };
+         ldap_memfree(attribute);
+         attribute = ldap_next_attribute(ld, entry, ber);
+      };
+
+      // skip to the next entry
+      entry = ldap_next_entry(ld, entry);
+   };
+   NSLog(@" ");
+
+   NSLog(@"   unbinding from LDAP server...");
+   ldap_unbind_ext_s(ld, NULL, NULL);
+	
+	return;
+}
+
+int my_ldap_sasl_interact_proc(LDAP *ld, unsigned flags, void *defaults,
+   void * sasl_interact)
+{
+	int               noecho;
+	int               challenge;
+   MyLDAPAuth      * defs = defaults;
+   const char      * dflt;
+	sasl_interact_t * interact = sasl_interact;
+
+   if (!(ld))
+      return(LDAP_PARAM_ERROR);
+
+NSLog(@"      entering my_ldap_sasl_interact_proc()");
+   while(interact->id != SASL_CB_LIST_END)
+   {
+      noecho    = 0;
+      challenge = 0;
+      dflt      = interact->defresult;
+      interact->result = NULL;
+      interact->len    = 0;
+      switch( interact->id )
+      {
+         case SASL_CB_GETREALM:
+            NSLog(@"         processing SASL_CB_GETREALM");
+            interact->result = defs->realm ? defs->realm : "";
+            interact->len    = strlen( interact->result );
+            break;
+         case SASL_CB_AUTHNAME:
+            NSLog(@"         processing SASL_CB_AUTHNAME");
+            interact->result = defs->authuser ? defs->authuser : "";
+            interact->len    = strlen( interact->result );
+            break;
+         case SASL_CB_PASS:
+            NSLog(@"         processing SASL_CB_PASS");
+            interact->result = defs->passwd ? defs->passwd : "";
+            interact->len    = strlen( interact->result );
+            noecho = 1;
+            break;
+         case SASL_CB_USER:
+            NSLog(@"         processing SASL_CB_USER");
+            interact->result = defs->user ? defs->user : "";
+            interact->len    = strlen( interact->result );
+            break;
+         case SASL_CB_NOECHOPROMPT:
+            NSLog(@"         processing SASL_CB_NOECHOPROMPT");
+            noecho = 1;
+            challenge = 1;
+            break;
+         case SASL_CB_ECHOPROMPT:
+            NSLog(@"         processing SASL_CB_ECHOPROMPT");
+            challenge = 1;
+            break;
+         default:
+            NSLog(@"         I don't know how to process this.");
+            break;
+      };
+      interact++;
+   };
+   NSLog(@"      exiting my_ldap_sasl_interact_proc()");
+
+   return(LDAP_SUCCESS);
+};
