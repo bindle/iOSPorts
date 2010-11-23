d508421a92fc7a85dc8c7f355aab15264d00490f
David M. Syzdek <syzdek@bindlebinaries.com>
2010-11-22 22:45:56 -0900

    Adding standard comments to iOSPorts-pkginfo.m

--- ports/iOSPorts/other/iOSPorts-pkginfo.m
+++ ports/iOSPorts/other/iOSPorts-pkginfo.m
@@ -1,8 +1,62 @@
 /*
- * Quick example to test dlsym()
- * build: gcc -W -Wall -Werror -o test-dlsym test-dlsym.c
- * Usage: ./test-dlsym openldap
+ *  iOS Ports Library
+ *  Copyright (c) 2010, Bindle Binaries
+ *  All rights reserved.
+ *
+ *  @BINDLE_BINARIES_BSD_LICENSE_START@
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are
+ *  met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Bindle Binaries nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ *  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BINDLE BINARIES BE LIABLE FOR
+ *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ *  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ *  SUCH DAMAGE.
+ *
+ *  @BINDLE_BINARIES_BSD_LICENSE_END@
  */
+/**
+ *  @file ports/iOSPorts/other/iOSPorts-pkginfo.m displays package information
+ */
+/*
+ *  Simple Build:
+ *     gcc -W -Wall -O2 -c iOSPorts-pkginfo.m
+ *     gcc -W -Wall -O2 -o iOSPorts-pkginfo   iOSPorts-pkginfo.o
+ *
+ *  GNU Libtool Build:
+ *     libtool --mode=compile gcc -W -Wall -g -O2 -c iOSPorts-pkginfo.m
+ *     libtool --mode=link    gcc -W -Wall -g -O2 -o iOSPorts-pkginfo iOSPorts-pkginfo.lo
+ *
+ *  GNU Libtool Install:
+ *     libtool --mode=install install -c iOSPorts-pkginfo /usr/local/bin/iOSPorts-pkginfo
+ *
+ *  GNU Libtool Clean:
+ *     libtool --mode=clean rm -f iOSPorts-pkginfo.lo iOSPorts-pkginfo
+ */
+#define _IOSPORTS_SRC_IOSPORTS_PKGINFO_M 1
+
+///////////////
+//           //
+//  Headers  //
+//           //
+///////////////
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,8 +65,25 @@
 #include <iOSPorts/iOSPortsTypes.h>
 #include <iOSPorts/iOSPortsCFuncs.h>
 
+
+//////////////////
+//              //
+//  Prototypes  //
+//              //
+//////////////////
+
 extern iOSPortsPKGListData iOSPortsPKGList[];
 
+
+/////////////////
+//             //
+//  Functions  //
+//             //
+/////////////////
+
+/// main statement
+/// @param[in]  argc  number of arguments passed to program
+/// @param[in]  argv  array of arguments passed to program
 int main(int argc, char * argv[])
 {
    const iOSPortsPKGData * datap;
@@ -38,3 +109,5 @@ int main(int argc, char * argv[])
 
    return(0);
 }
+
+/* end of source file */
